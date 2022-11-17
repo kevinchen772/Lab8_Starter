039c6c0414057b9d273e7816f0fd21104c44cc25
kevinchen772 <xic039@ucsd.edu>
2022-11-16 21:45:34 -0800

    lab 8

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 8 - Starter
 1. Correct answer: Within a Github action that runs whenever code is pushed. We are fitting automated test so we are not going to manually run them. Also, CI/CD stands for Continuous Integration / Continuous Delievery so that we cannot wait till the end to test our code. 
 2. No 
+3. No. Because this is not individual component, this feature requires us to check if the message is send and received by a user. It is more of an interaction.
+4. Yes. Because it only involves testing individual part. We only have to test if max length is working correctly. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('1 is not phonenumber', () => {
+    expect(functions.isPhoneNumber('1')).toBe(false);
+});
+
+test('00 is not phonenumber', () => {
+    expect(functions.isPhoneNumber('00')).toBe(false);
+});
+
+test('858-539-3399 is phonenumber', () => {
+    expect(functions.isPhoneNumber('858-539-3399')).toBe(true);
+});
+
+test('858-539-1122 is phonenumber', () => {
+    expect(functions.isPhoneNumber('858-539-1122')).toBe(true);
+});
+
+test('kevin@gmail.com is email', () => {
+    expect(functions.isEmail('kevin@gmail.com')).toBe(true);
+});
+
+test('kevin@outlook.com is email', () => {
+    expect(functions.isEmail('kevin@outlook.com')).toBe(true);
+});
+
+test('kevin.com is not email', () => {
+    expect(functions.isEmail('kevin.com')).toBe(false);
+});
+
+test('kevin77777.com is not email', () => {
+    expect(functions.isEmail('kevin77777.com')).toBe(false);
+});
+
+test('1234 is not a Strong Password', () => {
+    expect(functions.isStrongPassword('1234')).toBe(false);
+});
+
+test('qqq is not a Strong Password', () => {
+    expect(functions.isStrongPassword('qqq')).toBe(false);
+});
+
+test('q123qq is a Strong Password', () => {
+    expect(functions.isStrongPassword('q123qq')).toBe(true);
+});
+
+test('i1kdjflas is a Strong Password', () => {
+    expect(functions.isStrongPassword('i1kdjflas')).toBe(true);
+});
+
+test('11/11/2001 is a date', () => {
+    expect(functions.isDate('11/11/2001')).toBe(true);
+});
+
+test('11/11/2019 is a date', () => {
+    expect(functions.isDate('11/11/2019')).toBe(true);
+});
+
+test('11 11 2019 is not a date', () => {
+    expect(functions.isDate('11 11 2019')).toBe(false);
+});
+
+test('1111 is not a date', () => {
+    expect(functions.isDate('1111')).toBe(false);
+});
+
+test('1111 is not a Hex Color', () => {
+    expect(functions.isHexColor('1111')).toBe(false);
+});
+
+test('1 is not a Hex Color', () => {
+    expect(functions.isHexColor('1')).toBe(false);
+});
+
+test('AF1234 is a Hex Color', () => {
+    expect(functions.isHexColor('AF1234')).toBe(true);
+});
+
+test('FF0000 is a Hex Color', () => {
+    expect(functions.isHexColor('FF0000')).toBe(true);
+});
